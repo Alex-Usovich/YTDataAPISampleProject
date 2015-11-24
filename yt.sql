916c2570fc1d68f0cd28bb8137a701b94f0d094e
axeJustice <a.p.usovich@gmail.com>
2015-11-24 19:39:31 +0300

    commit changes

---
+++ yt.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.5.44-0+deb8u1 : Database - yt
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yt` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `yt`;
+
+/*Table structure for table `videos` */
+
+DROP TABLE IF EXISTS `videos`;
+
+CREATE TABLE `videos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_video` text,
+  `id_channel` text,
+  `video_title` text,
+  `channel_title` text,
+  `video_description` text,
+  `video_thumbnail` text,
+  `views_count` int(11) DEFAULT NULL,
+  `comments_count` int(11) DEFAULT NULL,
+  `likes_count` int(11) DEFAULT NULL,
+  `dislikes_count` int(11) DEFAULT NULL,
+  `subscribers_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
